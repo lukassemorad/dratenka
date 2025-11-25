dd8a61b8a7b675b88b48ef2b3264842ccea546ce
Lukas <semorad@techcrowd.cz>
2025-11-25 22:04:43 +0100

    project updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./*.html", "./pages/**/*.html", "./test/**/*.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'soap-pink': {
+          50: '#F7F7F7',  // Soft white/very light grey
+          100: '#F7A9C8', // Light bubble pink
+          500: '#E7499A', // Main bright pink
+          600: '#C21875', // Deep magenta/purple-pink
+          700: '#234A8C', // Dark blue (used in labels/strips)
+        }
+      }
+    },
   },
   plugins: [],
 }
